9286627e51088543fe31cd3f3ad1423001b6bce7
Cameron C <cameronmcobb.epsilon@gmail.com>
2021-04-03 15:06:47 -0500

    HTML pages
    Created HTML pages for index, login, and register. Entered validator schema for newUsers, which will probably need to be modified later.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const usersRouter = require("./Controllers/UserController");
 const projectsRouter = require("./Controllers/ProjectController");
 const openProjectsRouter = require("./Controllers/OpenProjectsController");
 
+const path = require("path");
 const PORT = 8005;
 
 app.use(express.json());
@@ -15,6 +16,9 @@ app.use('/users', usersRouter);
 app.use('/projects', projectsRouter);
 app.use('/openProjects', openProjectsRouter);
 
+app.use(express.static(path.join(__dirname, "public"), {
+    extensions: ['html'],
+}));
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
